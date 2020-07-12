591affe0d41d16165a37e99fadd398151c26711c
arjunItech <arjun_itech@yahoo.co.in>
2020-07-12 11:21:26 +0530

    release-1

--- src/Filter.java
+++ src/Filter.java
@@ -3,4 +3,10 @@ public class Filter {
 	System.out.println("new wickedly smart stuff");
 	System.out.println("new wickedly smart stuff");
 	
+	
+	public method newFeature() {
+		
+		System.out.println ("Hello Siyree");
+	}
+	
 }
--- src/com/must/newPackage/ParallelProcess.java
+++ src/com/must/newPackage/ParallelProcess.java
@@ -10,19 +10,19 @@ public class ParallelProcess {
 	public void variableCannotInitialized() {
 
 		int x;
-
+		int y;
 		final int initializationcompulsory = 0;
 
+		System.out.println("hello");
 		System.out.println("hello");
 
 	}
 
 	public static void main(String args[]) {
-		
+
 		long beforeUsedMem4 = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-	
-	
-		byte[] bytes ="Hello".getBytes();
+
+		byte[] bytes = "Hello".getBytes();
 		Path path = Paths.get("/Users/arjundas/Desktop/myfile1.bytes");
 		try {
 			Files.write(path, bytes);
@@ -30,14 +30,12 @@ public class ParallelProcess {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	for(int i=0;i<10;i++)
-	{
-		System.out.println("Hello"+i);
-		System.out.println("Hello"+i);
-		
-	}
-	
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello" + i);
+			System.out.println("Hello" + i);
+
+		}
+
 	}
 }
