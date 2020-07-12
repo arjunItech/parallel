0949c70b85d8f7d0fd49382cd328c2cecac91f8e
arjunItech <arjun_itech@yahoo.co.in>
2020-07-12 12:01:48 +0530

    ist commit second commit third commit fourth comiit trying out interactive rebase

--- src/Filter.java
+++ src/Filter.java
@@ -5,7 +5,14 @@ public class Filter {
 	
 	System.out.println("now cherry Pick");
 	System.out.println("now cherry next commit");
+	System.out.println("Interactive reabse");
+	System.out.println("first Commit");
+	System.out.println("Second Commit");
+	System.out.println("third Commit");
+	System.out.println("third Commit");
 	
+	System.out.println("fourth Commit");
+	System.out.println("fourth Commit");
 	
 	
 }
